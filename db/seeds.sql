e1ef58fb32dd412b78914e56c34b981abfc575cd
Elvy Kiung <kiungh22@gmail.com>
2019-06-30 21:16:53 -0700

    initialize file and set up db connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Barbecue burger', false);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaii burger', true);
